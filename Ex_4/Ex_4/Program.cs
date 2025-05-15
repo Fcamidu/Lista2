acf80fbbea73acb6311626941f65bbbb38ceb52b
Mateus <fcamidumateus345@gmail.com>
2025-05-14 21:53:41 -0300

    Add files via upload

---
+++ Ex_4/Ex_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double bas3;
+            double h;
+
+            Console.Write("Digite a altura do seu triangulo:");
+            h  = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a Base do seu triangulo:");
+            bas3 = double.Parse(Console.ReadLine());
+
+            if (bas3 >= 100)
+            {
+                Console.WriteLine("Terreno Muito Grande!!");
+            }
+            else
+            {
+                double area = (bas3 * h) / 2;
+                Console.WriteLine("A area do triangulo é:{0}", area);
+            }
+        }
+    }
+}
